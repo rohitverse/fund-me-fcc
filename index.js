2c7accdb9c1ab1275cea5a4fc28a67af5e5200fa
rohitverse <rohitkr23072001@gmail.com>
2023-03-16 13:09:25 +0530

    prettier fix:index.js

--- index.js
+++ index.js
@@ -1,19 +1,97 @@
-import {ethers} from "./ethers-5.6.esm.min" ;
-import {abi,contractAddress} from "./constants"
+import { ethers } from "./ethers-5.6.esm.min.js";
+import { abi, contractAddress } from "./constants.js"
 
-const connectButton=document.getElementById("connectButton");
-const withdrawButton=document.getElementById("withdrawButton")
-const fundButton=document.getElementById("fundButton")
-const balanceButton=document.getElementById("balanceButton")
+const connectButton = document.getElementById("connectButton");
+const withdrawButton = document.getElementById("withdrawButton")
+const fundButton = document.getElementById("fundButton")
+const balanceButton = document.getElementById("balanceButton")
+
+connectButton.onclick = connect;
+withdrawButton.onclick = withdraw;
+fundButton.onclick = fund;
+balanceButton.onclick = getBalance;
 
 
-
-    async () => {
-        if (typeof window.ethereum !== "undefined") {
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        try {
             await window.ethereum.request({ method: "eth_requestAccounts" })
-            document.getElementById("connectButton").innerHTML = ("Connected")
 
-        } else {
-            document.getElementById("connectButton").innerHTML = ("Please Install Metamask")
+        } catch (error) {
+            console.log(error)
         }
+        connectButton.innerHTML = ("Connected")
+
+        const accounts = await ethereum.request({ method: "eth_Accounts" })
+        console.log(accounts)
+    } else {
+        connectButton.innerHTML = ("Please Install Metamask")
     }
+}
+
+async function withdraw() {
+    console.log("Withdrawing.....")
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        await provider.send("eth_requestAccounts", [])
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.withdraw()
+            await listenForTransactionMine(transactionResponse, provider)
+            // await transactionResponse.wait(1)
+        }
+        catch (error) {
+            console.log(error)
+        }
+    } else {
+        withdrawButton.innerHTML = "Please Install MetaMask"
+    }
+}
+
+async function fund() {
+    const ethAmount = document.getElementById("ethAmount").value
+    console.log(`funding with ${ethAmount}.....`)
+
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.fund({ value: ethers.utils.parseEther(ethAmount), })
+            await listenForTransactionMine(transactionResponse, provider)
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        fundButton.innerHTML = "Please Install Metamask"
+    }
+}
+
+async function getBalance() {
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        try {
+            const balance = await provider.getBalance(contractAddress)
+            console.log(ethers.utils.formatEther(balance))
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        balanceButton.innerHTML = "Please Install Metamask"
+    }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}`)
+    return new Promise((resolve, reject) => {
+        try {
+            provider.once(transactionResponse.hash, (transactionReceipt) => {
+                console.log(`Completed with ${transactionReceipt.confirmations} Confirmations`)
+                resolve()
+            })
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
